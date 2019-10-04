d77b096b0d09921f454cee140034361265c71e3a
Bill Reed <billnreed@gmail.com>
2019-10-04 12:49:25 -0400

    intro animation

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,32 @@
 import '../../assets/styles/reset.scss';
 import '../../assets/styles/bill-loves-beth.scss';
 
-console.log('whoa')
+const timeline = [
+  {
+    items: [{
+      selector: '.logo-container',
+      addClass: 'reveal',
+    }],
+    duration: 3000,
+  },
+  {
+    items: [
+      {
+        selector: '.logo-container',
+        addClass: 'move',
+      }
+    ],
+    duration: 1000,
+  }
+];
+
+
+let runningTime = 0;
+timeline.forEach(entry => {
+  entry.items.forEach(item => {
+    const el = document.querySelector(item.selector);
+    setTimeout(() => el.classList.add(item.addClass), runningTime);
+  });
+  runningTime += entry.duration;
+});
+

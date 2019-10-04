1a158bd542d04cf089362b9ec9f45704af420aa8
Bill Reed <billnreed@gmail.com>
2019-10-04 13:55:51 -0400

    fixed random

--- src/scripts/quote-picker.ts
+++ src/scripts/quote-picker.ts
@@ -2,7 +2,7 @@ import { quotes } from './quotes';
 
 export class QuotePicker {
   pickQuote() {
-    const index = Math.round(Math.random() * quotes.length) - 1;
+    const index = Math.ceil(Math.random() * quotes.length) - 1;
 
     return quotes[index];
   }
